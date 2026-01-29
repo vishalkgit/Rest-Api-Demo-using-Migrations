011fadf998847e50ed64f16c4edbb68906787913
vishalkgit <kalevishal1012@gmail.com>
2026-01-29 15:57:39 +0530

    Remaining Methods

--- Rest Api Demo using Migrations/Controllers/EmployeeController.cs
+++ Rest Api Demo using Migrations/Controllers/EmployeeController.cs
@@ -41,13 +41,34 @@ namespace Rest_Api_Demo_using_Migrations.Controllers
             return Ok(res);
         }
 
-        [HttpPost]
+        [HttpPut]
         [Route("{Eid:int}")]
         public IActionResult updateEmp(int Eid,Employee emp)
         {
-            var res=dbContext.Employees.Update(emp);
+            var allemp = dbContext.Employees.Find(Eid);
+            if(allemp is null)
+            {
+                return NoContent();
+            }
+            //allemp.Eid = emp.Eid;
+            allemp.EName = emp.EName;
+            allemp.Salary = emp.Salary;
             dbContext.SaveChanges();
-            return Ok(res);
+            return Ok(allemp);
+        }
+
+        [HttpDelete]
+        [Route("{Eid:int}")]
+        public IActionResult DeleteEmp(int Eid)
+        {
+            var emp = dbContext.Employees.Find(Eid);
+            if(emp is null)
+            {
+                return NotFound();
+            }
+            var res=dbContext.Employees.Remove(emp);
+            dbContext.SaveChanges();
+            return Ok();
         }
 
 
